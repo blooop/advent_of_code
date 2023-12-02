94abd14ea1c7ec77a2f885c39a67c381d0697de8
Austin Gregg-Smith <blooop@gmail.com>
2023-12-02 17:40:05 +0000

    Feature/day2 (#2)
    * chore: copy over day1 code at template
    
    * feat: solve day2
    
    * fix: linting
    
    * fix: autoformatted and ruff --fix all code

---
+++ advent_of_code/2023/day02/solution.py
@@ -0,0 +1,82 @@
+from __future__ import annotations
+from advent_of_code.utils.io import readstrings, get_example
+from dataclasses import dataclass
+from typing import List
+import numpy as np
+
+
+@dataclass
+class Cubeset:
+    red: int = 0
+    green: int = 0
+    blue: int = 0
+
+    @staticmethod
+    def from_line(line: str) -> List[Cubeset]:
+        game_sets = line[line.find(":") + 1 :].split(";")
+        return [Cubeset.from_set(s) for s in game_sets]
+
+    @staticmethod
+    def from_set(cube_set: str) -> Cubeset:
+        cubes = [s.strip() for s in cube_set.split(",")]
+        d = {}
+        for c in cubes:
+            print(c)
+            num, col = c.split(" ")
+            d[col] = int(num)
+        return Cubeset(**d)
+
+    def possible(self, bag: Cubeset) -> bool:
+        return self.red <= bag.red and self.green <= bag.green and self.blue <= bag.blue
+
+    def as_vec(self):
+        return np.array([self.red, self.green, self.blue])
+
+
+elf_bag = Cubeset(12, 13, 14)
+
+
+def solve1(file):
+    result = 0
+    for i, line in enumerate(readstrings(file)):
+        games = Cubeset.from_line(line)
+        possible = True
+        for g in games:
+            if not g.possible(elf_bag):
+                possible = False
+                break
+        if possible:
+            result += i + 1
+    return result
+
+
+def solve2(file):
+    result = 0
+    for line in readstrings(file):
+        games = Cubeset.from_line(line)
+        games_mat = np.vstack([g.as_vec() for g in games])
+        min_vals = np.max(games_mat, 0)
+        powers = min_vals[0] * min_vals[1] * min_vals[2]
+        result += powers
+        print(games_mat)
+        print(min_vals)
+        print(powers)
+    return result
+
+
+if __name__ == "__main__":
+    res = solve1(get_example(1, __file__))
+    print(res)
+    assert res == 8
+
+    res = solve1(get_example("input", __file__))
+    print(res)
+    assert res == 2727
+
+    res = solve2(get_example(2, __file__))
+    print(res)
+    assert res == 2286
+
+    res = solve2(get_example("input", __file__))
+    print(res)
+    assert res == 56580
--- advent_of_code/utils/io.py
+++ advent_of_code/utils/io.py
@@ -1,3 +1,10 @@
+import pathlib
+
+
 def readstrings(path: str):
     with open(path, encoding="utf-8") as f:
         return [line.rstrip("\n") for line in f]
+
+
+def get_example(example_number: int, file_in_folder: str):
+    return pathlib.Path(file_in_folder).parent / f"{example_number}.in"
